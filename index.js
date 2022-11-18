f103e32489dc7dc3bbb4f85624b9a279f86506e0
olivia <opierez@gmail.com>
2022-11-18 18:27:38 -0500

    commit tree 0380452c681c8e566ebd68bc97648964a3bfe0e6

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if (num <= 400) {
+    return 'This one is on me!';
+  } else if (num >= 400 && num <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (num > 2000 && num < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (num > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
+} 
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return 'Bye.';
+  }
 }
